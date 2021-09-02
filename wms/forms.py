3a4145521fc85cdcdc791587f978c49c84d2f0ef
linguini1 <matteo.golin@gmail.com>
2021-09-02 12:24:01 -0400

    Finished item form.

--- wms/forms.py
+++ wms/forms.py
@@ -21,9 +21,26 @@ class WarehouseForm(FlaskForm):
 
 # Item form
 class ItemForm(FlaskForm):
-    name = StringField()
-    price = DecimalField()
-    cost = DecimalField()
-    size = IntegerField()
-    lowThreshold = IntegerField()
+    name = StringField('Name',
+                       validators=[DataRequired(),
+                                   Length(min=2, max=15)],
+                       render_kw={"placeholder": "Name"})
+    price = DecimalField('Price',
+                         validators=[DataRequired(),
+                                     NumberRange(min=0.01, max=99999.99)],
+                         render_kw={"placeholder": "Price"},
+                         places=2)
+    cost = DecimalField('Cost',
+                        validators=[DataRequired(),
+                                    NumberRange(min=0.01, max=99999.99)],
+                        render_kw={"placeholder": "Cost"},
+                        places=2)
+    size = IntegerField('Size',
+                        validators=[DataRequired(),
+                                    NumberRange(min=1, max=500)],
+                        render_kw={"placeholder": "Size"})
+    lowThreshold = IntegerField('Low Stock Threshold',
+                                validators=[DataRequired(),
+                                            NumberRange(min=1, max=500)],
+                                render_kw={"placeholder": "Low Stock Threshold"})
 
